9d7a8694e6608bc1725fc686cbeab4f8e24ca061
hdelc <hdelc02@gmail.com>
2025-12-26 23:15:01 -0600

    Use selector in SelectFromChoices

--- src/main/java/org/carden/lockoutgames/goal/factory/base/SelectFromChoices.java
+++ src/main/java/org/carden/lockoutgames/goal/factory/base/SelectFromChoices.java
@@ -3,44 +3,47 @@ package org.carden.lockoutgames.goal.factory.base;
 import org.carden.lockoutgames.LockoutGames;
 import org.carden.lockoutgames.goal.GoalDifficulty;
 import org.carden.lockoutgames.goal.IMutableGoal;
+import org.carden.lockoutgames.goal.factory.selector.MapSelector;
+import org.carden.lockoutgames.goal.factory.selector.SingleSelector;
 
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public abstract class SelectFromChoices<E> extends BaseGoalFactory {
-    private final Map<E, GoalDifficulty> choices;
+    private final Map<E, GoalDifficulty> difficultyMap;
+    private final MapSelector<E, GoalDifficulty> selector;
+
+    protected SelectFromChoices(Map<E, GoalDifficulty> choices, Predicate<E> choiceFilter) {
+        this.difficultyMap = choices;
+        this.selector = new MapSelector<>(choices, choiceFilter, this::isValidDifficulty);
+    }
+
+    protected SelectFromChoices(Set<E> choices, GoalDifficulty difficulty, Predicate<E> choiceFilter) {
+        this(
+                choices.stream().collect(Collectors.toMap((e) -> e, (e) -> difficulty)),
+                choiceFilter
+        );
+    }
 
     protected SelectFromChoices(Map<E, GoalDifficulty> choices) {
-        this.choices = choices;
+        this(choices, (x) -> true);
     }
 
     protected SelectFromChoices(Set<E> choices, GoalDifficulty difficulty) {
-        this.choices = choices.stream().collect(Collectors.toMap((e) -> e, (e) -> difficulty));
+        this(choices.stream().collect(Collectors.toMap((e) -> e, (e) -> difficulty)));
     }
 
     @Override
     protected final IMutableGoal makeGoalHook() {
-        E value = selectRandomFrom(this.filteredChoices());
-        return this.makeGoalFor(value, this.choices.get(value));
+        Map.Entry<E, GoalDifficulty> target = this.selector.select();
+        return this.makeGoalFor(target.getKey(), target.getValue());
     }
 
     protected abstract IMutableGoal makeGoalFor(E value, GoalDifficulty difficulty);
 
     @Override
     protected boolean canGenerateGoalHook() {
-        return !this.filteredChoices().isEmpty();
-    }
-
-    /**
-     * Subclasses can further filter choices by overriding.
-     * @return choices that may be selected for the game
-     */
-    protected Set<E> filteredChoices() {
-        return this.choices.keySet().stream().filter((value) -> this.isValidDifficulty(this.choices.get(value))).collect(Collectors.toSet());
-    }
-
-    private E selectRandomFrom(Set<E> choices) {
-        Random rng = LockoutGames.getRng();
-        return List.copyOf(choices).get(rng.nextInt(choices.size()));
+        return this.selector.canSelect();
     }
 }
